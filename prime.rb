b3dd4495b1a73b5ecb3a53f7a2253849a43d70be
Chhaian Pin <chhaianpin@hotmail.com>
2019-01-07 00:57:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(number)
+  if (number <= 1)
+    return false
+  end
+  
+  for num in 2...number
+    if (number % num == 0)
+      return false
+    end
+  end
+  
+  return true
+end
