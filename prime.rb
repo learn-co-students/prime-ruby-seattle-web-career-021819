20ec9232413a11e12a896a478aa0cab881cd6752
Joshua M Loran <joshua.loran@gmail.com>
2019-01-09 20:04:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  if (number <= 1)
+    return false
+  end
+  for item in 2...number
+    if(number % item == 0)
+      return false
+    end
+  end
+  return true
+end
