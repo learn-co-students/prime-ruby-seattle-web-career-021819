17fbbfee7ab7e39dfa7ca446d36d682c87872133
github username <github email address>
2019-01-12 23:33:07 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+if number <= 0
+return false
+end
+
+if number == 1
+  return false
+end
+
+ (2..(number - 1)).each do |x|
+    return false if number % x == 0
+end
+true
+end
