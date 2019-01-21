1a732f8df7322afc872590efbaeb4635b3226cbd
adriennemiller <millera.92@gmail.com>
2019-01-20 19:51:33 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(number)
+    if (number <= 1)
+    return false
+  end
+
+  array = (2...number).to_a
+  array.each do |number_in_array|
+  if (number % number_in_array == 0)
+  return false
+  end
+end
+
+return true
+end
