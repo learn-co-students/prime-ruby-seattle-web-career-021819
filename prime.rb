2d6fa8ea904da5b4b80837f0d5c6d242a9e69968
rtboudreau <rtboudreau@gmail.com>
2019-01-23 11:34:27 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(number)
+
+	if (number <= 1)
+		return false
+	end
+
+	for a in 2...number
+		if (number % a == 0)
+			return false
+		end
+	end
+
+	return true
+
+		
+end
+
