1aa6ffeccca0ea8468d65531b6189b9c85e2dd2a
github username <github email address>
2019-02-02 13:45:31 -0800

    prime lab solution

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+array = (2...num).to_a
+
+if num <= 1
+  return false
+end 
+
+for i in array
+  if num % i == 0
+    return false
+  end
+end
+  return true
+end
