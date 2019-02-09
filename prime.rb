08f964b5f660c63fe8ba946ed13ac2a500f8f3ba
Kim Heidorn <heidorkn@gmail.com>
2019-02-09 16:26:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  if num < 2 
+    false
+  elsif num == 3 || num == 2 
+    true
+  elsif (2...num-1).any?{|i| num % i == 0}
+    false
+  else
+    true
+  end
+end
